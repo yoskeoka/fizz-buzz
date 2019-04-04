95b2520f1fbe06af8b04b91033e1d987241d8e24
yoskeoka <y.okamoto@gemcook.com>
2019-04-04 17:01:37 +0900

    [update] fizz

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ fn main() {
 }
 
 fn fizzbuzz(n: i32) -> String {
+    if n % 3 == 0 {
+        return "fizz".to_string();
+    }
     return n.to_string();
 }
 
@@ -10,5 +13,5 @@ fn fizzbuzz(n: i32) -> String {
 fn fizzbuzz1() {
     assert_eq!("1", fizzbuzz(1));
     assert_eq!("2", fizzbuzz(2));
-    // assert_eq!("fizz", fizzbuzz(3));
+    assert_eq!("fizz", fizzbuzz(3));
 }
